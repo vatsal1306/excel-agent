5920a78f568698599689644422a5b057c8597fa8
vatsal1306 <vatsal1399@gmail.com>
2026-03-13 22:16:26 +0530

    load environment variables from .env file and set output directory to version 12

--- src/__init__.py
+++ src/__init__.py
@@ -1,4 +1,10 @@
 import os
 
+from dotenv import dotenv_values
+
 ROOT_DIR = os.path.dirname(os.path.abspath(os.path.dirname(__file__)))
 OUTPUT_ROOT = os.path.join(ROOT_DIR, 'data', 'outputs', 'v12')
+
+dotenv_path = os.path.join(ROOT_DIR, '.env')
+
+envs = dotenv_values(dotenv_path, verbose=True)
