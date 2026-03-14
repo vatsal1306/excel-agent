dc131482a8c4ebb6ab5d9457f4560530961f0fe3
Megha Tamrakar <megha.tamrakar@Meghas-MacBook-Air.local>
2026-03-15 02:23:14 +0530

    add comments

--- src/crypto.py
+++ src/crypto.py
@@ -5,9 +5,15 @@ from src.config import TOKEN_ENCRYPTION_KEY
 fernet = Fernet(TOKEN_ENCRYPTION_KEY.encode())
 
 
-def encrypt(text: str) -> str:
+def encrypt_token(text: str) -> str:
+    """
+    Encrypt token before storing in DB
+    """
     return fernet.encrypt(text.encode()).decode()
 
 
-def decrypt(text: str) -> str:
+def decrypt_token(text: str) -> str:
+    """
+     Decrypt token before using in API calls
+     """
     return fernet.decrypt(text.encode()).decode()
